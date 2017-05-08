1cf67deda5ce8555ebdfa704691b27dbff79da5d
exchris <2687547643@qq.com>
2017-05-08 10:23:40 +0800

    RestfulApi

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/5/8 0008
+ * Time: 上午 10:15
+ */
+header("Content-type:text/html;charset=utf-8");
+require __DIR__.'/lib/User.php';
+require __DIR__.'/lib/Article.php';
+$pdo = require __DIR__.'/lib/db.php';
+$user = new User($pdo);
+var_dump($user->register("admin2","admin2"));
+$article = new article($pdo);
+/*var_dump($article->create('文章标题1','文章内容1',1));
+var_dump($article->create('文章标题2','文章内容2',1));
+var_dump($article->create('文章标题3','文章内容3',1));
+var_dump($article->create('文章标题4','文章内容4',1));*/
+//var_dump($article->getList(1,1,1));
